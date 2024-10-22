ec8b77b7ed082d240a780285de61f9ecbe808e4c
Jesus Adolfo Parra Parra <jesusadolfo.parra@platform.sh>
2024-10-22 15:22:18 -0400

    more solutions

---
+++ solutions/ch3-3.9-?.go
@@ -0,0 +1,127 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 61.
+//!+
+
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"math/cmplx"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	var xForm = 0
+	var yForm = 0
+	var zoom = 0
+	var err error
+
+	if err = r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	if r.Form.Get("x") != "" {
+		xForm, err = strconv.Atoi(r.Form.Get("x"))
+		if err != nil {
+			log.Fatal("can't not convert to int X")
+		}
+	}
+
+	if r.Form.Get("y") != "" {
+		yForm, err = strconv.Atoi(r.Form.Get("y"))
+		if err != nil {
+			log.Fatal("can't not convert to int Y")
+		}
+	}
+
+	if r.Form.Get("zoom") != "" {
+		zoom, err = strconv.Atoi(r.Form.Get("zoom"))
+		if err != nil {
+			log.Fatal("can't not convert to int zoom")
+		}
+	}
+
+	fmt.Printf("Ignoring x:%d , y:%d , zoom: %d\n", xForm, yForm, zoom)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(w, img) // NOTE: ignoring errors
+
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
+
+//!-
+
+// Some other interesting functions:
+
+func acos(z complex128) color.Color {
+	v := cmplx.Acos(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+
+func sqrt(z complex128) color.Color {
+	v := cmplx.Sqrt(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{128, blue, red}
+}
+
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//
+//	= z - (z^4 - 1) / (4 * z^3)
+//	= z - (z - 1/z^3) / 4
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return color.Gray{255 - contrast*i}
+		}
+	}
+	return color.Black
+}
